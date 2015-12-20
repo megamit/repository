7ac045167709f8e26c35d5fe9245e263bc76c66f
megamit <mistermitchell@outlook.com>
2015-12-20 18:01:43 +0000

    emote data addeD

---
+++ skype-emotes/jsonbuilder.py
@@ -0,0 +1,17 @@
+import json
+import os, os.path
+from PIL import Image
+
+data = {}
+for root, _, files in os.walk("C:\\Dev\\Dump\\repo\\repository\\skype-emotes"):
+    for f in files:
+        fullpath = os.path.join(root, f)
+        if os.path.isfile(fullpath):
+            if fullpath[-4:]==".bmp":
+                i = Image.open(fullpath)
+                data[f[:-4]] = {"url":"http://megamit.github.io/repository/skype-emotes/"+f,"type":"sprite","steps": int(i.height/i.width)}
+    file = open("skypeemotedata.json","w")
+    file.write(json.dumps(data));
+    file.close();
+    
+            
