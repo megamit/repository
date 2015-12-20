cd118faf2868be7acc33a6d428dceed9098f08ac
megamit <mistermitchell@outlook.com>
2015-12-20 18:04:37 +0000

    New lines Added

--- skype-emotes/jsonbuilder.py
+++ skype-emotes/jsonbuilder.py
@@ -9,7 +9,7 @@ for root, _, files in os.walk("C:\\Dev\\Dump\\repo\\repository\\skype-emotes"):
         if os.path.isfile(fullpath):
             if fullpath[-4:]==".bmp":
                 i = Image.open(fullpath)
-                data[f[:-4]] = {"url":"http://megamit.github.io/repository/skype-emotes/"+f,"type":"sprite","steps": int(i.height/i.width)}
+                data[f[:-4]] = {"url":"https://megamit.github.io/repository/skype-emotes/"+f,"type":"sprite","steps": int(i.height/i.width)}
     file = open("skypeemotedata.json","w")
     file.write(json.dumps(data));
     file.close();
